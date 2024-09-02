5c4d37138398a4d5ed98bec0263f2e3a6d91f157
mukanika <k511218m@outlook.jp>
2024-09-02 17:58:14 +0900

    ファイル追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -2,50 +2,21 @@
 <html lang="ja">
 
 <head>
-<<<<<<< HEAD
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Attendance Management</title>
-  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/common.css') }}">
-=======
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Contact Form</title>
   <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
   <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
->>>>>>> 1d5a0c1 (ファイル追加)
   @yield('css')
 </head>
 
 <body>
   <header class="header">
     <div class="header__inner">
-<<<<<<< HEAD
-      <div class="header-utilities">
-        <a class="header__logo" href="/">
-          Attendance Management
-        </a>
-        <nav>
-          <ul class="header-nav">
-            <li class="header-nav__item">
-              <a class="header-nav__link" href="/mypage">マイページ</a>
-            </li>
-            <li class="header-nav__item">
-              <form>
-                <button class="header-nav__button">ログアウト</button>
-              </form>
-            </li>
-          </ul>
-        </nav>
-      </div>
-=======
       <a class="header__logo" href="/">
         Contact Form
       </a>
->>>>>>> 1d5a0c1 (ファイル追加)
     </div>
   </header>
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\MukaController;
-use App\Http\Controllers\ConfirmController;
-
-=======
 use App\Http\Controllers\ContactController;
->>>>>>> 1d5a0c1 (ファイル追加)
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,12 +16,7 @@ Route::get('/', function ()
 {
     return view('welcome');
 });
-<<<<<<< HEAD
-Route::get('/muka', [MukaController::class, 'index']);
-Route::get('/confirm', [ConfirmController::class, 'index']);
-=======
 
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
 Route::post('/contacts', [ContactController::class, 'store']);
->>>>>>> 1d5a0c1 (ファイル追加)
